bdfe617fc3f07fcc1b29d81c5992fdfb0f0fdd21
AndrMshk <andrmshk1@gmail.com>
2022-03-09 19:09:21 +0200

    init

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+type MessageProps = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+export function Message({avatar, message, name, time}: MessageProps) {
+    return (
+        <div className='main'>
+            <img src={avatar} alt="" width={50}/>
+            <div className='text'>
+                <div className="name">{name}</div>
+                <div className="message">{message}</div>
+                <div className="time">{time}</div>
+            </div>
+
+        </div>
+    )
+}
+
